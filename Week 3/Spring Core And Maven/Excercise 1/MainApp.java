4ac95947aaff348f7049de7c803dbf9fcb7d13d0
DivyanshPoddar31 <dpoddar3108@gmail.com>
2025-07-04 17:12:15 +0530

    Add files via upload

---
+++ Week 3/Spring Core And Maven/Excercise 1/MainApp.java
@@ -0,0 +1,23 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter book name to add: ");
+        String bookName = scanner.nextLine();
+
+        bookService.addBook(bookName);
+
+        System.out.println("Book has been successfully processed through the system.");
+        scanner.close();
+    }
+}
